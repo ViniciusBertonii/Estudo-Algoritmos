0edd0549c6e482b52d4456116c3154fc702b3fee
ViniciusBertonii <vinicius.marcos98@edu.pucrs.br>
2024-04-15 10:52:53 -0300

    teste

--- MyLinkedList/src/MinhaLinkedList.java
+++ MyLinkedList/src/MinhaLinkedList.java
@@ -1,5 +1,5 @@
 public class MinhaLinkedList {
-    /*public boolean add (int element){
+public boolean add (int element){
         if (iEmpity()) {
                         
         }else{
@@ -7,20 +7,5 @@ public class MinhaLinkedList {
         }
         
     }
-    */
-
-    public int indexOf(int element){
-        Nodo navegador=head;
-        int count;
-
-        while (navegador!=null) {
-            if (navegador.valor==element) {
-                return count;
-            }
-            navegador=navegador.prox;
-
-            count++;
-        }
-    }
-
+    
 }
