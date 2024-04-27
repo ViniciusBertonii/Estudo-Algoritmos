9a17b8068af3c3b445e013379a562655b38adef8
Marcos Vinicius Bertoni <vinicius.marcos98@edu.pucrs.br>
2024-04-27 15:48:35 -0300

    rec

---
+++ EstudoRecursividade/Recursividade/src/ExercRecursividade.java
@@ -0,0 +1,11 @@
+public class ExercRecursividade {
+
+    public static String reveString(String texto) {
+        String reversa = new String("");
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
